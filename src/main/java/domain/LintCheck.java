ba1f40f3fcb934e6cff30a6330b59dfa0af76b1f
adlerbn <adlerbn@rose-hulman.edu>
2024-02-08 13:12:39 -0500

    Added pre-adapter rendition of check interface

---
+++ src/main/java/domain/LintCheck.java
@@ -0,0 +1,19 @@
+package domain;
+
+import org.objectweb.asm.tree.ClassNode;
+
+import java.util.List;
+
+/**
+ * Interface for individual linting checks
+ */
+interface LintCheck {
+    /**
+     * BE AWARE: "classes" will change types to a list of classNodeAdapters in the future
+     * BE AWARE: make sure if you implement this before the change any code that deals with classNodes is easy to change
+     * Method to run a lint check on classes
+     * @param classes a list of class nodes to lint over
+     * @return a list of strings that hold the linting messages
+     */
+    List<String> runLintCheck(List<ClassNode> classes);
+}
