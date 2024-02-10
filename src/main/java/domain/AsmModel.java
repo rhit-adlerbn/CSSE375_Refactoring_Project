28ea5833fcc5de2b2468d8c7d0dc716bb2cc1f3b
adlerbn <adlerbn@rose-hulman.edu>
2024-02-10 13:35:59 -0500

    Added getName to AsmModel

--- src/main/java/domain/AsmModel.java
+++ src/main/java/domain/AsmModel.java
@@ -5,6 +5,8 @@ import org.objectweb.asm.ClassVisitor;
 public abstract class AsmModel {
     private  ClassVisitor node;
 
-
+    public String getName() {
+        return node.getClass().getName();
+    }
 
 }
