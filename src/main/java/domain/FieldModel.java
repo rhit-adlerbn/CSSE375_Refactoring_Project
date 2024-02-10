435efee1e58ba6756a710a37d7e9fedd5b37dd15
adlerbn <adlerbn@rose-hulman.edu>
2024-02-10 14:13:14 -0500

    added basic field access modifiers

--- src/main/java/domain/FieldModel.java
+++ src/main/java/domain/FieldModel.java
@@ -1,10 +1,34 @@
 package domain;
 
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.FieldNode;
+
+import java.util.List;
 
 public class FieldModel {
     private FieldNode node;
     public FieldModel(FieldNode node) {
         this.node = node;
+    }
+    public String getName() {
+        return node.name;
+    }
+    public boolean isPublic() {
+        return isAccessModifier(Opcodes.ACC_PUBLIC);
+    }
+    public boolean isPrivate() {
+        return isAccessModifier(Opcodes.ACC_PRIVATE);
+    }
+    public boolean isProtected() {
+        return isAccessModifier(Opcodes.ACC_PROTECTED);
+    }
+    public boolean isStatic() {
+        return isAccessModifier(Opcodes.ACC_STATIC);
+    }
+    public boolean isFinal() {
+        return isAccessModifier(Opcodes.ACC_FINAL);
+    }
+    private boolean isAccessModifier(int opCode){
+        return (node.access & opCode) != 0;
     }
 }
