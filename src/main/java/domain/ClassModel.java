1501073a889aefe7d422487e5d3af80e317d622f
adlerbn <adlerbn@rose-hulman.edu>
2024-02-10 13:46:41 -0500

    added getSuperName and getInterfaces()

--- src/main/java/domain/ClassModel.java
+++ src/main/java/domain/ClassModel.java
@@ -2,11 +2,19 @@ package domain;
 
 import org.objectweb.asm.tree.ClassNode;
 
+import java.util.List;
+
 public class ClassModel {
     private ClassNode node;
 
     public String getName() {
         return node.name;
+    }
+    public List<String> getInterfaces() {
+        return node.interfaces;
+    }
+    public String getSuperName() {
+        return node.superName;
     }
 
 
