78b2c3781d8fb26b97821dda027c0464911962e9
adlerbn <adlerbn@rose-hulman.edu>
2024-02-10 13:53:57 -0500

    added boolean access methods to ClassModel

--- src/main/java/domain/ClassModel.java
+++ src/main/java/domain/ClassModel.java
@@ -1,5 +1,6 @@
 package domain;
 
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.ClassNode;
 
 import java.util.List;
@@ -16,6 +17,29 @@ public class ClassModel {
     public String getSuperName() {
         return node.superName;
     }
-
+    public boolean isPublic() {
+        return isAccessModifer(Opcodes.ACC_PUBLIC);
+    }
+    public boolean isPrivate() {
+        return isAccessModifer(Opcodes.ACC_PRIVATE);
+    }
+    public boolean isProtected() {
+        return isAccessModifer(Opcodes.ACC_PROTECTED);
+    }
+    public boolean isStatic() {
+        return isAccessModifer(Opcodes.ACC_STATIC);
+    }
+    public boolean isFinal() {
+        return isAccessModifer(Opcodes.ACC_FINAL);
+    }
+    public boolean isAbstract() {
+        return isAccessModifer(Opcodes.ACC_ABSTRACT);
+    }
+    public boolean isInterface() {
+        return isAccessModifer(Opcodes.ACC_INTERFACE);
+    }
+    private boolean isAccessModifer(int opCode){
+        return (node.access & opCode) != 0;
+    }
 
 }
