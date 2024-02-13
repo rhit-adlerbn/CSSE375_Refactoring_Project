ff6336e70d6d141f00d87cd6773660297d1fd3c3
adlerbn <adlerbn@rose-hulman.edu>
2024-02-13 18:27:29 -0500

    Implemented Singleton Check

---
+++ src/main/java/domain/SingletonCheck.java
@@ -0,0 +1,44 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.regex.Pattern;
+
+//	Private constructor
+//	Private static variable of the same class that is the only instance of the class.
+//	Public static method that returns the instance of the class
+
+public class SingletonCheck implements LintCheck{
+    public SingletonCheck(){}
+    public ArrayList<String> runLintCheck(ArrayList<ClassModel> classes) {
+        ArrayList<String> msgs = new ArrayList<String>();
+        for (ClassModel c : classes) {
+            msgs.add(runStaticCheck(c));
+        }
+        return msgs;
+    }
+
+    private String runStaticCheck(ClassModel classMod) {
+        boolean privateInit = false;
+        boolean privateInst = false;
+        boolean publicGetInst = false;
+        String name = classMod.getName();
+        for (MethodModel m : classMod.getMethods()) {
+            if ((m.getName().equals("getInstance") || m.getName().equals("getInst")) && m.isPublic())
+                publicGetInst = true;
+            if (m.getName().equals("<init>") && m.isPrivate())
+                privateInit = true;
+        }
+        for (FieldModel f : classMod.getFields()) {
+            if (f.isPrivate() && typeMatches(f, name) && (f.getName().equals("inst") || f.getName().equals("instance")))
+                privateInst = true;
+        }
+        if (privateInit && privateInst && publicGetInst) return name + " is a Singleton";
+        else return name + " is not a Singleton";
+    }
+    private boolean typeMatches(FieldModel field, String className){
+
+        return (field.getInstType().equals(className));
+    }
+}
