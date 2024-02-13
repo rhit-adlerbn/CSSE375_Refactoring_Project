540c8d33b5205b3f6ba4114d6eb25521573da70a
fieldaj1 <fieldaj1@rose-hulman.edu>
2024-02-13 13:58:14 -0500

    CLI progress

--- src/main/java/presentation/Main.java
+++ src/main/java/presentation/Main.java
@@ -2,9 +2,11 @@ package presentation;
 
 import domain.LintCheck;
 import domain.TemplateCheck;
+import org.objectweb.asm.tree.ClassNode;
 
-import java.util.HashMap;
-import java.util.Scanner;
+import java.text.ParseException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -18,20 +20,40 @@ public class Main {
         String filePath = scanner.nextLine();
 
         // TODO: generate .class files from given package
+        List<ClassNode> classes = new ArrayList<>();
 
         // Show user all available lint checks
         HashMap<Integer, LintCheck> checks = new HashMap<Integer, LintCheck>();
         checks.put(1, new TemplateCheck());
+
         System.out.println("\n\nBelow are the numbers associated with each lint check\n" +
                 "------------------------------------------------------------");
         System.out.println(checks + "\n");
 
-        System.out.print("Please enter the number of the check you want to perform: ");
+        System.out.print("Please enter the numbers of the checks you want to perform (separate with spaces): ");
         String checkNumber = scanner.nextLine();
+        List<Integer> checkCommands = convertInput(checkNumber);
 
-        // TODO: run specified checks
+
+        for(Integer i : checkCommands) {
+            checks.get(i).runLintCheck(classes);
+        }
+
+        // TODO: add option to run all checks
 
         scanner.close();
 
+    }
+
+    /**
+     * Converts an input string to a list of Integers
+     * Restriction: Numbers in string must be space-separated
+     * @param s
+     * @return
+     */
+    private static List<Integer> convertInput(String s) {
+        return Arrays.stream(s.split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
