52cc569e798d71e80023e8abb062da96227368cb
adlerbn <adlerbn@rose-hulman.edu>
2024-02-13 17:27:55 -0500

    Changed to relative path

--- src/main/java/presentation/Main.java
+++ src/main/java/presentation/Main.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        ArrayList<ClassModel> test= ASMAdapter.parseASM( "C:\\Users\\taskand\\IdeaProjects\\project-202420-s3-team15-202420\\src\\main\\java\\domain");
+        ArrayList<ClassModel> test = ASMAdapter.parseASM( "src\\main\\java\\domain");
         for(ClassModel javaClass: test){
             System.out.println(javaClass.getName());
 
