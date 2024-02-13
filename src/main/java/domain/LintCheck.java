15c120a3c67f8ba44ebe9857f376c3eff92f5467
adlerbn <adlerbn@rose-hulman.edu>
2024-02-13 17:03:15 -0500

    Made lintCheck public

--- src/main/java/domain/LintCheck.java
+++ src/main/java/domain/LintCheck.java
@@ -7,7 +7,7 @@ import java.util.List;
 /**
  * Interface for individual linting checks
  */
-interface LintCheck {
+public interface LintCheck {
     /**
      * Method to run a lint check on classes
      * @param classes a list of class models to lint over
