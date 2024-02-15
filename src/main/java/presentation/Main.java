e30dc0f219db2d7547a07fdf3b046a832db214c8
fieldaj1 <fieldaj1@rose-hulman.edu>
2024-02-15 10:17:03 -0500

    Add-all option to CLI

--- src/main/java/presentation/Main.java
+++ src/main/java/presentation/Main.java
@@ -20,33 +20,32 @@ public class Main {
         // Read the input string provided by the user
         String filePath = scanner.nextLine();
 
-        // TODO: generate .class files from given package (need to merge Adapter_and_Model branch)
-
         List<ClassModel> classes = new ArrayList<>();
         classes = ASMAdapter.parseASM(filePath);
 
         // Show user all available lint checks
         HashMap<Integer, LintCheck> checks = new HashMap<Integer, LintCheck>();
-        checks.put(1, new TemplateCheck());
+        checks.put(2, new TemplateCheck());
         // TODO: add all checks to hashmap
 
         System.out.println("\n\nBelow are the numbers associated with each lint check\n" +
                 "------------------------------------------------------------");
+        System.out.println("1 = Run All Checks");
         System.out.println(checks + "\n");
 
         System.out.print("Please enter the numbers of the checks you want to perform (separate with spaces): ");
         String checkNumber = scanner.nextLine();
         List<Integer> checkCommands = convertInput(checkNumber);
 
-        // TODO: add option to run all checks
+        if(checkCommands.get(0) == 1) {
+            checkCommands = new ArrayList<>();
+            checkCommands.addAll(checks.keySet());
+        }
 
         for(Integer i : checkCommands) {
             List<String> output = checks.get(i).runLintCheck(classes);
             for(String s : output) System.out.println(s);
         }
-
-
-
 
         scanner.close();
 
