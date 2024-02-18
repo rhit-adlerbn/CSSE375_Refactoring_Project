df2e53663ba32f6d0910daa63ffb65495cba2509
fieldaj1 <fieldaj1@rose-hulman.edu>
2024-02-18 14:14:50 -0500

    Privatizer done

--- src/main/java/domain/Privatizer.java
+++ src/main/java/domain/Privatizer.java
@@ -1,21 +1,20 @@
 package domain;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 public class Privatizer {
 
     /**
+     * For each class, makes all methods and fields private
+     * that are not accessed by any outside class
      *
-     * @param classes a list of class models to lint over
-     * @return list of Strings denoting variables that could be private,
-     * but aren't
+     * @param classes a list of class models to privatize
      */
     public void privatize(List<ClassModel> classes) {
         List<String> violations = new ArrayList<>();
         for(ClassModel c : classes) {
-            violations.addAll(privatizeClass(c, classes));
+            privatizeClass(c, classes);
         }
     }
 
@@ -23,28 +22,31 @@ public class Privatizer {
      *
      * @param subject the class under examination
      * @param classes the full list of classes in the package
-     * @return List of strings denoting any non-private variables in c that
-     * are not accessed by any other classes
+     * privatizes all fields and methods of subject that aren't accessed
+     * by any other class
      */
     private void privatizeClass(ClassModel subject, List<ClassModel> classes) {
-        Collection<String> violations = new ArrayList<>();
         List<ClassModel> allOtherClasses = new ArrayList<>(classes); allOtherClasses.remove(subject);
         List<FieldModel> fields = subject.getFields();
+        List<MethodModel> methods = subject.getMethods();
 
         for(FieldModel f : fields) {
-            if(!f.isPrivate() && !isAccessed(f, subject, allOtherClasses)){
+            if(!f.isPrivate() && !isAccessedField(f, subject, allOtherClasses)) f.privatize();
+        }
 
-            }
+        for(MethodModel m : methods) {
+            if(!m.isPrivate() && !isAccessedField(m, subject, allOtherClasses)) m.privatize();
         }
     }
 
     /**
      *
      * @param f the field in question
+     * @param subject the owner of f
      * @param allOtherClasses
      * @return true iff the field f is accessed by at least one of allOtherClasses
      */
-    public boolean isAccessed(FieldModel f, ClassModel subject, List<ClassModel> allOtherClasses) {
+    private boolean isAccessedField(FieldModel f, ClassModel subject, List<ClassModel> allOtherClasses) {
         for(ClassModel c : allOtherClasses) {
             List<MethodModel> methods = c.getMethods();
             for(MethodModel m : methods) {
@@ -53,6 +55,30 @@ public class Privatizer {
                 for(int i = 0; i < s; i++) {
                     AbstractInsnModel insn = instructions.get(i);
                     if(insn.isFieldInsn() && insn.getFieldInsnModel().matchesField(f, subject)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param m the method in question
+     * @param subject the owner of m
+     * @param allOtherClasses
+     * @return true iff the method m is accessed by at least one of allOtherClasses
+     */
+    private boolean isAccessedMethod(MethodModel m, ClassModel subject, List<ClassModel> allOtherClasses) {
+        for(ClassModel c : allOtherClasses) {
+            List<MethodModel> methods = c.getMethods();
+            for(MethodModel method : methods) {
+                InstructionModel instructions = method.getInstructions();
+                int s = instructions.getSize();
+                for(int i = 0; i < s; i++) {
+                    AbstractInsnModel insn = instructions.get(i);
+                    if(insn.isMethodInsn() && insn.getMethodInsnModel().matchesMethod(m, subject)) {
                         return true;
                     }
                 }
