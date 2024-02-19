abae4d0d3abf0335ac980b1f469a61a00773270e
FireballXd2 <taskandeniz@gmail.com>
2024-02-18 20:12:50 -0500

    PlantUML

--- src/main/java/domain/PlantUml.java
+++ src/main/java/domain/PlantUml.java
@@ -1,6 +1,7 @@
 package domain;
 
 import org.objectweb.asm.Type;
+import org.testng.internal.junit.ArrayAsserts;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -69,9 +70,11 @@ public class PlantUml implements LintCheck{
 
     public String CreateUML(ClassModel classNode){
         String uml = "";
-        uml += "+class " + classNode.getName().substring(classNode.getName().lastIndexOf("/") + 1) + "{\n";
+        String names = classNode.getName().substring(classNode.getName().lastIndexOf("/") + 1);
+        uml += "+class " + names + "{\n";
         //List<MethodNode> methods = (List<MethodNode>) classNode.methods;
-
+        String depend = "";
+        List<MethodModel> methods = classNode.getMethods();
         List<FieldModel> fields = classNode.getFields();
         HashMap<String, Integer> Primary =  new HashMap<String, Integer>();
         Primary.put("String", 1);
@@ -87,7 +90,8 @@ public class PlantUml implements LintCheck{
                 fieldType = Type.getType(field.getSignature());
             }
             String name = fieldType.getClassName();
-            System.out.println("\n" + name +"\n");
+            name = name.substring(name.lastIndexOf(".") + 1);
+            //System.out.println("\n" + name +"\n");
             if(name.contains("Array")){
                 for(String key: Primary.keySet()){
                     if(name.contains(key)){
@@ -97,12 +101,37 @@ public class PlantUml implements LintCheck{
             }
             if(Primary.containsKey(name)){
                 uml += getAccess(field.getAccess()) + field.getName() + ": " + name + "\n" ;
+            }else{
+                depend += names + "-->" + name +"\n";
             }
+
         }
-        uml+="}";
+        for(MethodModel method: methods){
+            if(!method.getName().equals("<init>")) {
+                uml += getAccess(method.isPrivate()) + method.getName() + "(";
+                List<LocalVarModel> params = method.getLocalVars();
+                for (LocalVarModel param : params) {
+                    if(!param.getName().equals("this")) {
+                        uml += param.getName() + ",";
+                    }
+                }
+                uml = uml.substring(0, uml.length()-1);
+                uml += "):" + method.getReturnType() + "\n";
+            }
+
+        }
+
+        uml+="}\n";
+        uml += depend;
         return uml;
     }
 
+
+    public static String getAccess(boolean acces){
+        if(acces)
+            return "-";
+        return "+";
+    }
     public static String getAccess(int acces){
         switch (acces){
             case (1):
---
+++ src/test/java/domain/PlantUMLTest.java
@@ -0,0 +1,69 @@
+package domain;
+import datasource.ASMAdapter;
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlantUMLTest {
+
+
+    private String filePath = "src/test/resources/PlantUML";
+
+    ArrayList<ClassModel> classesUnderTest;
+    {
+        classesUnderTest = ASMAdapter.parseASM(filePath);
+    }
+
+    ClassModel Simple = classesUnderTest.get(0);
+    ClassModel second = classesUnderTest.get(1);
+    ClassModel third = classesUnderTest.get(2);
+
+    @Test
+    public void TestSimple() throws IOException {
+
+        LintCheck check = new PlantUml();
+        ArrayList<ClassModel> classes = new ArrayList<>();
+        classes.add(Simple);
+        List<String> actual = check.runLintCheck(classes);
+        List<String> expected = new ArrayList<String>();
+        //expected.add("Subject");
+        expected.add("+class Test1{\n-First: int\n-Second: int\n}\n");
+        //expected.add("Not Observer Pattern");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestExternal() throws IOException {
+
+        LintCheck check = new PlantUml();
+        ArrayList<ClassModel> classes = new ArrayList<>();
+        classes.add(second);
+        List<String> actual = check.runLintCheck(classes);
+        List<String> expected = new ArrayList<String>();
+
+        expected.add("+class Test2{\n-name: double\n}\nTest2-->Test1\n");
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestComplex() throws IOException {
+
+        LintCheck check = new PlantUml();
+        ArrayList<ClassModel> classes = new ArrayList<>();
+        classes.add(third);
+        List<String> actual = check.runLintCheck(classes);
+        List<String> expected = new ArrayList<String>();
+
+        expected.add("+class Test3{\n-Method1(arg1):void\n+Method2(arg1,arg2):String\n}\nTest3-->Test2\nTest3-->Test1\n");
+
+        Assert.assertEquals(expected, actual);
+    }
+
+
+
+
+}
